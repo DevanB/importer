e8c2c4529c28143af81381260761cc60630239da
Jesse Storimer <jesse@jadedpixel.com>
2009-02-25 11:27:42 -0500

    Actually record error message.

--- app/models/import.rb
+++ app/models/import.rb
@@ -108,7 +108,7 @@ class Import < ActiveRecord::Base
       begin
         parse
       rescue NameError => e
-       import_errors << "There was an error parsing your import file."
+       import_errors << "There was an error parsing your import file. #{e.message}"
       rescue CSV::IllegalFormatError => e
         import_errors << "There was an error parsing your import file. Your import file is not a valid CSV file."      
       rescue REXML::ParseException => e
