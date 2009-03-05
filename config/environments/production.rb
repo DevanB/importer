333d33f37b6622b0b940a5f459b724242b308923
Jesse Storimer <jesse@jadedpixel.com>
2009-03-05 08:51:36 -0500

    Fixed forgotten changes in production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,13 +11,11 @@ config.cache_classes = true
 config.whiny_nils = true
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = true
+config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 
 # Use a different cache store in production
 # config.cache_store = :mem_cache_store
-
-config.log_level = :debug
 
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
