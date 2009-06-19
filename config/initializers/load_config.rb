71a7ed84b2a33d3f397bbe68ef68ce6eed3e7cfb
Jesse Storimer <jesse@jadedpixel.com>
2009-06-19 13:47:19 -0400

    Load config vars from ENV in production mode.

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,1 +1,5 @@
-APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/importer.yml")[RAILS_ENV]
+if Rails.env.production?
+  APP_CONFIG = ENV
+else
+  APP_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/importer.yml")[RAILS_ENV]
+end
