df9734c0a61a4c8a08e3a9cdad8af12cd5e09cab
Jesse Storimer <jesse@jadedpixel.com>
2009-06-19 14:16:05 -0400

    Revert "Removed xml-mapping from vendor and added as a gem dependency."
    This reverts commit 1cf280d387ded32e615ffa8764ab7cb1b678bad6.

--- config/environment.rb
+++ config/environment.rb
@@ -12,12 +12,13 @@ RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  
+  config.load_paths += [RAILS_ROOT + '/vendor/xml-mapping/lib']
+  
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Uncomment to use default local time.
   config.time_zone = 'UTC'
-  
-  config.gem 'xml-mapping'
 
   config.action_controller.session = {
     :session_key => '_Importer_session',
