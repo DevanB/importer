5e6f8a83e3b25b4a7963c49cf6ced57d3d846a33
Jesse Storimer <jesse@jadedpixel.com>
2009-06-19 12:37:42 -0400

    Updated shopify_app plugin.

--- vendor/plugins/shopify_app/lib/shopify_login_protection.rb
+++ vendor/plugins/shopify_app/lib/shopify_login_protection.rb
@@ -3,12 +3,13 @@ module ShopifyLoginProtection
   def shopify_session
     if session[:shopify]
       begin
+        # session[:shopify] set in LoginController#finalize
         ActiveResource::Base.site = session[:shopify].site
         yield
       ensure 
         ActiveResource::Base.site = nil
       end
-    else      
+    else            
       session[:return_to] = request.path
       redirect_to :controller => 'login'      
     end
