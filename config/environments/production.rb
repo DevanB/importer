a5afb89758ca0a1b0411b965e238606251eecc9f
Jesse Storimer <jesse@jadedpixel.com>
2009-07-02 10:44:47 -0400

    Switch environment to use :smtp instead of :sendmail.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,10 +20,6 @@ config.action_controller.perform_caching             = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host                  = "http://assets.example.com"
 
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
-config.action_mailer.delivery_method        = :sendmail
-
 config.after_initialize do
   ENV['from_address']                 = "Shopify's Importer <importer@shopifyapps.com>"
 end
