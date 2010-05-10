e7e760a8253180b510210506db419c9d03a8d645
Jesse Storimer <jstorimer@gmail.com>
2010-05-10 17:01:47 -0400

    Revert "tmp migration"
    This reverts commit 0e7bd706155d5d33e1c2ffd498272885f9dd39ae.

--- db/migrate/20100510205800_add_content_back_to_imports.rb
+++
@@ -1,9 +0,0 @@
-class AddContentBackToImports < ActiveRecord::Migration
-  def self.up
-    add_column :imports, :content, :text
-  end
-
-  def self.down
-    remove_column :imports, :content
-  end
-end
